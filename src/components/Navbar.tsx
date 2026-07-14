f9ed98a91c82648e1294e24f38dbb81420bdb4fb
Daniel Olowoniyi <olowoniyidaniel@gmail.com>
2026-07-14 18:54:31 +0100

    fix: update Navbar component and favicon
    - Adjusted Navbar component layout for improved responsiveness and consistency.
    - Updated favicon.ico for better branding.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,15 +6,14 @@ import { Button } from '@/components/ui/button'
 export default function Navbar() {
   return (
     <nav className="w-full max-w-[1512px] mx-auto flex justify-between items-center px-4 md:px-[64px] lg:px-[128px] py-4 md:py-[40px]">
-      <div className="flex items-center">
+      <div className="flex w-[120px] items-center sm:w-[150px] lg:w-[180px]">
         <Image
           priority
           alt="DevFest Ilorin Logo"
-          className="h-auto w-[120px] sm:w-[150px] lg:w-[180px]"
-          height={60}
+          className="h-auto w-full"
+          height={54}
           src="/svg/logo.svg"
-          style={{ height: 'auto' }}
-          width={180}
+          width={186}
         />
       </div>
       <Link
