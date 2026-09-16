3391e000bad7cba407c0b9f54508388837c624ee
SirMartel22 <oyinloyematthew6@gmail.com>
2026-09-16 13:18:41 +0100

    feat(recap): add DevFest recap horizontal auto-scrolling gallery

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from '@/components/Navbar'
 import Hero from '@/components/Hero'
 import EventStatus from '@/components/EventStatus'
+import Scrollable from '@/components/scrollable'
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Navbar />
       <Hero />
       <EventStatus />
+      <Scrollable />
     </main>
   )
 }
